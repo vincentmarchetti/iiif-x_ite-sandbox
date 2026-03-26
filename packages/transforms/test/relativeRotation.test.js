7ef5b65bc90b23c28b8471b0d009dbe969f9fdc3
Vincent Marchetti <vmarchetti@kshell.com>
2026-03-26 15:58:06 -0400

    More complete testing, this can be regarded as complete testing for now.

--- packages/transforms/test/relativeRotation.test.js
+++ packages/transforms/test/relativeRotation.test.js
@@ -5,18 +5,21 @@ import {S,R,T} from "./setup/build_manifesto_transform.js";
 import {Vector3} from "threejs-math";
 
 
-const testCases=[
-    [T(3.0,-2.,2.)],
-    [T(0.0,2.0,0.0)],
-];
+
 
 describe("relativeRotation test", function(){
 
+    const testCases=[
+        [T(3.0,-2.,2.)],
+        [T(0.0,2.0,0.0)],
+        [T(-1,4,2)],        // this one is above the aimAt
+        [T(3, -5, -7)]
+    ];
     
     testCases.forEach(function(testcase){
         const [manifesto_base] = testcase;
-        it(`base : ${manifesto_base}`, function(){        
-            const manifesto_aimAt = T(0,0,0);
+        const manifesto_aimAt = T(-1,0,2);
+        it(`base : ${manifesto_base} aimAt : ${manifesto_aimAt}`, function(){                    
         
             const base = Transform.from_manifesto_transform(manifesto_base);
             const aimAt= Transform.from_manifesto_transform(manifesto_aimAt);
@@ -44,7 +47,8 @@ describe("relativeRotation test", function(){
             // camera_x_axis perpendicular to world y_axis
             expect( camera_x_axis.dot(y_axis)).to.almost.equal(0.0);
             
-            // unless the direction is straight up or down (parallel to y_axis)
+            // unless the direction is straight up or down (parallel
+            // or antiparallel to y_axis)
             // want camera_y_axis to have a positive component along the y_axis
             if ( new Vector3().crossVectors(unit_direction, y_axis).length() > 1.0-7){
                 expect(y_axis.dot(camera_y_axis)).to.be.above(0.0);
@@ -52,4 +56,12 @@ describe("relativeRotation test", function(){
             
         });
     });
+        
+    it(`edge case: base and aimAt at same location`, function(){
+        const location = Transform.from_manifesto_transform( T(-3.45, 0.456,1.0) );
+        expect(location).to.exist;
+        const result = relativeRotation(location, location);
+        expect(result).to.equal(null);
+    });
+
 });
