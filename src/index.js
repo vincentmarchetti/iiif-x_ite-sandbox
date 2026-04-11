a666d18765b0f01068130d270c93e145ae48e187
Vincent Marchetti <vmarchetti@kshell.com>
2026-04-11 19:51:40 -0400

    restored the "sandbox" or playground style web page "# Please enter the commit message for your changes. Lines starting

--- src/index.js
+++ src/index.js
@@ -1,54 +1,53 @@
 import {fetch_manifest_json, initialize_x_ite_viewer} from "@kshell/manifest-viewer";
 import * as manifesto  from "@kshell/manifesto-prezi4" ;
+import * as jsonlint   from "@prantlf/jsonlint";
 
 
-
-/*
-    handle_manifest_json is called when an object has
-    been parsed from manifest json text.
-    
-    It is intended to be defined in the global scope so that
-    it can be invoked dynamically over the course of a pages lifescycle,
-    such as 
-        -- on document loading, when a manifest is fetched from a url defined 
-           in window.location
-        -- on user request to fetch a manifest from a url entered into HTML input
-        -- on user request when manifest text is entered or copied into HTML input
-*/
-
-const FALLBACK_MANIFEST_URL= "https://spri-open-resources.s3.us-east-2.amazonaws.com/iiif3dtsg/"
-                            +"manifests/BlenderPluginDemo_20251110/demo_v2.json";
-async function load_manifest(){
-    const data = await ( async () => {
-        try{
-            return await fetch_manifest_json(window.location, FALLBACK_MANIFEST_URL);
-        }  catch (error){
-            const message = ( (e) => {
-                if (e instanceof Error ) return e.message;
-                return String(e);
-            })(error);
-            window.alert(message);
-            console.error(message);
-            return null;
-        }
-    })();
-    
-    if (data != null){
-        const manifest = manifesto.buildManifest(data, {});
-        document.dispatchEvent( new CustomEvent("new_manifest", { "detail" : {"manifest" : manifest }}) );
-    } else {
-        console.error(`load_manifest: no url provided to load`);
+async function load_manifest_text(){
+    const manifestText = document.querySelector("textarea#manifest-text").value;
+    console.info(`manifest text: ${manifestText.length} characters`);
+    try{
+        const json     = jsonlint.parse(manifestText);
+        const manifest = new manifesto.Manifest(json, {});
+        clear_parsing_result();
+        await document.dispatchEvent( new CustomEvent("new_manifest", { "detail" : {"manifest" : manifest }}) );
     }
+    catch (error ){
+        console.info(`jsonlint parse failed with ${error}`);
+        show_parsing_error(error);
+        return;
+    }
+    return;
 }
 
+const PARSING_RESULT = "load-result-message";
+function clear_parsing_result(){
+    const result_div = document.getElementById( PARSING_RESULT );
+    result_div.innerText = "";
+    result_div.className = "success-parsing";
+}
 
+function show_parsing_error( errorMessage ){
+    const result_div = document.getElementById( PARSING_RESULT );
+    result_div.innerHTML = `<pre>${errorMessage.toString().trim()}</pre>`;
+    result_div.className = "failed-parsing";
+}
 
 document.addEventListener("DOMContentLoaded", async () => {
-    // first check that the X3D object is defined in this global scope, have
-    // been loaded with the x_ite library
-    if (X3D === undefined){
-        throw new Error(`DOMContentLoaded hander: X3D undefined`);
-    }
+    document
+    .querySelector("button#load-manifest-from-text")
+    .addEventListener("click",  async () => {
+        await load_manifest_text();
+    });
+    console.debug("DOMContentLoaded listener added to window");
+    
+    // Developer note 4/11/2026 : The variable X3D is imported into global scope 
+    // by the index.html script element loadeding x_ite.js
     initialize_x_ite_viewer(X3D);
-    await load_manifest();
-})
+    // if the text already has a manifest, then try to load it
+    // the length text is intended to allow putting something
+    // like "Insert Manifest here"
+    if (document.querySelector("textarea#manifest-text").value.length > 32)
+        await load_manifest_text();
+});    
+
